2ebae8dc073d30d1a9525d3417a39b1fa33ce007
CodingNoJam <hanwoodww@gmail.com>
2021-07-16 23:33:34 +0900

    update

---
+++ src/main/java/org/minideliveryproject/MiniDeliveryProjectApplication.java
@@ -0,0 +1,13 @@
+package org.minideliveryproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDeliveryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniDeliveryProjectApplication.class, args);
+    }
+
+}
