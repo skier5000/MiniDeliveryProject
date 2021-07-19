07828006a6e53e810e2584fb32f763d0357bd439
Junbeom Lee <skier5000@naver.com>
2021-07-19 16:00:33 +0900

    FranchiseMstRepository, FranchiseMstRepositoryImpl 추가

---
+++ src/main/java/org/minideliveryproject/application/domain/repository/FranchiseMstRepository.java
@@ -0,0 +1,15 @@
+package org.minideliveryproject.application.domain.repository;
+
+import org.minideliveryproject.application.domain.entity.FranchiseMst;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Repository
+public interface FranchiseMstRepository {
+
+    List<FranchiseMst> findAll();
+    Optional<FranchiseMst> findByFranchiseName(String franchiseName);
+}
