ec45ba7421d0f2f3b0c569fd46a2792b1456686c
Junbeom Lee <skier5000@naver.com>
2021-07-19 14:25:53 +0900

    변수 주석 수정

--- src/main/java/org/minideliveryproject/application/domain/entity/FranchiseMst.java
+++ src/main/java/org/minideliveryproject/application/domain/entity/FranchiseMst.java
@@ -14,8 +14,8 @@ public class FranchiseMst {
     @Column(name = "franchise_mst_seq")
     private Long seq;
 
-    private String franchiseName;
-    private String franchiseIssue;
+    private String franchiseName; // 프랜차이즈 명
+    private String franchiseIssue; // 프랜차이즈 이슈사항
 
     @Embedded
     private CommonColumn commonColumn;
