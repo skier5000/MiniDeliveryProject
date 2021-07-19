660379f70f457a0cc1dd45a24e876f63b1aa8ccd
Junbeom Lee <skier5000@naver.com>
2021-07-19 15:31:10 +0900

    Food Type enum 수정

--- src/main/java/org/minideliveryproject/application/domain/entity/FoodType.java
+++ src/main/java/org/minideliveryproject/application/domain/entity/FoodType.java
@@ -2,5 +2,14 @@ package org.minideliveryproject.application.domain.entity;
 
 
 public enum FoodType {
-    KOREAN_FOOD, CHINESE_FOOD, JAPANESE_FOOD, CHICKEN, SNACK
+    KOREAN_FOOD,    // 한식
+    CHINESE_FOOD,   // 중식
+    JAPANESE_FOOD,  // 일식
+    CHICKEN,        // 치킨
+    SNACK,          // 분식
+    PIZZA,          // 피자
+    PORKFEET,       // 족발
+    BOSSAM,         // 보쌈
+    FOODBOX,        // 도시락
+    FASTFOOD        // 패스트푸드
 }
