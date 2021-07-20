762bbe97bd43c277e1d7bf1c364b967280848fba
Junbeom Lee <skier5000@naver.com>
2021-07-20 11:53:36 +0900

    ItemMst spring data jpa 추가

---
+++ src/main/java/org/minideliveryproject/application/domain/repository/ItemMstRepository.java
@@ -0,0 +1,18 @@
+package org.minideliveryproject.application.domain.repository;
+
+import org.minideliveryproject.application.domain.entity.ItemMst;
+import org.minideliveryproject.application.domain.entity.StoreMst;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public interface ItemMstRepository {
+
+//    public List<ItemMst> findAll();
+//    public ItemMst findBySeq(Long seq);                                 // 아이템 시퀀스 조회
+//    public Optional<ItemMst> findByItemName(String itemName);           // 아이템 명 조회
+//    public Optional<ItemMst> findByItemCategory(String itemCategory);   // 아이템 카테고리 조회
+
+}
