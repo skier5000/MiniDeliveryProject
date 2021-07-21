101d9195dcc69d3b7388fec5aa8fffcd5883329b
JunbeomLee <skier5000@naver.com>
2021-07-21 15:23:37 +0900

    단일 인터페이스로 수정

--- src/main/java/org/minideliveryproject/application/domain/repository/ItemMstRepository.java
+++ src/main/java/org/minideliveryproject/application/domain/repository/ItemMstRepository.java
@@ -2,13 +2,14 @@ package org.minideliveryproject.application.domain.repository;
 
 import org.minideliveryproject.application.domain.entity.ItemMst;
 import org.minideliveryproject.application.domain.entity.StoreMst;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import java.util.List;
 import java.util.Optional;
 
-public interface ItemMstRepository {
+public interface ItemMstRepository extends JpaRepository<ItemMst, Long> {
 
     public List<ItemMst> findAll();
     public Long findBySeq(Long seq);                                 // 아이템 시퀀스 조회
