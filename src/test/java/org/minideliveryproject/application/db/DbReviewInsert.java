d909030981da2be28e4bc39d11fe00262ef3ac78
JunbeomLee <skier5000@naver.com>
2021-07-22 15:56:03 +0900

    Review Insert 테스트코드 추가 (게시물 형태로 데이터 받음)

--- src/test/java/org/minideliveryproject/application/db/DbReviewInsert.java
+++ src/test/java/org/minideliveryproject/application/db/DbReviewInsert.java
@@ -1,6 +1,7 @@
 package org.minideliveryproject.application.db;
 
 import org.junit.jupiter.api.Test;
+import org.minideliveryproject.application.domain.entity.Review;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,7 +14,10 @@ public class DbReviewInsert {
     @Test
     @Rollback(value = false)
     public void 리뷰데이터생성() {
+        /* given */
 
+        /* when */
 
+        /* then */
     }
 }
