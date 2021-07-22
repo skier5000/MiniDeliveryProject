f28166049ce0bb8a705ad02bac650002d12d0dd1
JunbeomLee <skier5000@naver.com>
2021-07-22 14:27:35 +0900

    결제방식 수정

--- src/main/java/org/minideliveryproject/application/domain/entity/embeded/PaymentType.java
+++ src/main/java/org/minideliveryproject/application/domain/entity/embeded/PaymentType.java
@@ -3,7 +3,7 @@ package org.minideliveryproject.application.domain.entity.embeded;
 public enum PaymentType {
     CARD,          // 신용/체크카드
     NAVERPAY,      // 네이버페이
-    PREPARING,     // 카카오페이
+    KAKAOPAY,     // 카카오페이
     TOSS,          // 토스
     DIRECTLYCARD,  // 만나서카드결제
     DIRECTLYCASH   // 만나서현금결제
