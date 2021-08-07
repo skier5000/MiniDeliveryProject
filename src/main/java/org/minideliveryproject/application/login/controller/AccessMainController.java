b842bb5dbb9ba623a2d4518f5ba5305d5df1b480
Lee JunBeon <skier5000@naver.com>
2021-08-07 15:26:50 +0900

    html, css, js 파일 디렉토리 변경 및 간단한 화면 접근 Controller 추가

--- src/main/java/org/minideliveryproject/application/login/controller/AccessMainController.java
+++ src/main/java/org/minideliveryproject/application/login/controller/AccessMainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Optional;
 
 
@@ -128,4 +129,14 @@ public class AccessMainController {
         return viewPage;
     }
 
+
+    @GetMapping("/test")
+    public ModelAndView htmlTest() throws IOException {
+        log.info("AccessMainController::htmlTest called");
+
+        ModelAndView viewPage = new ModelAndView();
+        viewPage.setViewName("layout/defaultLayout");
+
+        return viewPage;
+    }
 }
