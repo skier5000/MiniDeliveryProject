73ebfdb120166f0c271cafcbfdb6ba9e4b9d002d
Lee JunBeon <skier5000@naver.com>
2021-08-12 21:07:22 +0900

    UserMstRepositoryImpl (Spring Data JPA) 수정

--- src/main/java/org/minideliveryproject/application/domain/repository/UserMstRepositoryImpl.java
+++ src/main/java/org/minideliveryproject/application/domain/repository/UserMstRepositoryImpl.java
@@ -1,13 +1,17 @@
 package org.minideliveryproject.application.domain.repository;
 
+import org.minideliveryproject.application.domain.entity.ItemMst;
 import org.minideliveryproject.application.domain.entity.UserMst;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserMstRepositoryImpl extends JpaRepository<UserMst, Long> {
-    public List<UserMst> findAll();
+    public Optional<UserMst> findBySeq(Long seq);             // 유저 sequence 조회
+    public Optional<UserMst> findByUserId(String userId);     // 유저 id 조회
+    public Optional<UserMst> findByUserName(String userName); // 유저 name 조회
 
 }
