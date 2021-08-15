3b8962d06186b9cc8bea065d59d85e8a58180e55
Lee JunBeon <skier5000@naver.com>
2021-08-15 16:55:21 +0900

    crud back단 기능 추가

--- src/main/java/org/minideliveryproject/application/platform/controller/PlatformStoreMgtController.java
+++ src/main/java/org/minideliveryproject/application/platform/controller/PlatformStoreMgtController.java
@@ -6,6 +6,7 @@ import org.minideliveryproject.application.domain.entity.StoreMst;
 import org.minideliveryproject.application.platform.service.PlatformMainService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -25,17 +26,17 @@ import java.util.List;
  * 2021.08.14.		LJB			최초작성
  */
 
-
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("/storeMgt")
 @Slf4j
+@RequestMapping("/platform/storeMgt")
 public class PlatformStoreMgtController {
 
     private final PlatformMainService platformMainService;
 
     /**
      * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * /platform/storeMgt/franchise
      * @return
      */
     @GetMapping("/franchise")
@@ -43,12 +44,73 @@ public class PlatformStoreMgtController {
         return "platform/storeMgt/franchise";
     }
 
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 조회버튼
+     * @param franchiseCode
+     * @param franchiseStoreName
+     * @param franchiseStoreCity
+     * @param gugun
+     * @return
+     */
     @ResponseBody
-    @PostMapping("/franchise/search")
-    public List<StoreMst> franchiseSearch(@RequestParam("??") String dd) {
-        List<StoreMst> franchiseStoreAllList = platformMainService.selectFranchiseStoreAllList();
+    @GetMapping("/franchise/search")
+    public List<StoreMst> franchiseSearch(
+            @RequestParam(value = "franchiseCode", required = false) String franchiseCode,
+            @RequestParam(value = "franchiseStoreName", required = false) String franchiseStoreName,
+            @RequestParam(value = "franchiseStoreCity", required = false) String franchiseStoreCity,
+            @RequestParam(value = "gugun", required = false) String gugun
+    ) {
+        log.info("PlatformStoreMgtController::franchiseSearch called");
+        List<StoreMst> franchiseStoreSearchList = platformMainService.selectFranchiseStoreAllList();
+
+        return franchiseStoreSearchList;
+    }
+
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 등록버튼
+     */
+    @PostMapping("/franchise/create")
+    public List<StoreMst> franchiseCreate(
+            @RequestParam(value = "createFranchiseStoreList", required = true) List<StoreMst> createFranchiseStoreList
+    ) {
+        log.info("PlatformStoreMgtController::franchiseCreate called");
+        List<StoreMst> franchiseStoreAllList = platformMainService.createFranchiseStoreAllList(createFranchiseStoreList);
 
         return franchiseStoreAllList;
+    }
+
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 객체에서 일부분 수정
+     * 수정버튼
+     * @param updateFranchiseStoreList
+     * @return
+     */
+    @PatchMapping("/franchise/update")
+    public List<StoreMst> franchiseUpdate(
+            @RequestParam(value = "updateFranchiseStoreList", required = true) List<StoreMst> updateFranchiseStoreList
+    ) {
+        log.info("PlatformStoreMgtController::franchiseUpdate called");
+        platformMainService.updateFranchiseStoreAllList(updateFranchiseStoreList);
+
+        return updateFranchiseStoreList;
+    }
+
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * DELETE_TYPE = NO 수정
+     * 삭제버튼
+     */
+    @PatchMapping("/franchise/delete")
+    public List<StoreMst> franchiseDelete(
+            @RequestParam(value = "deleteFranchiseStoreList", required = true) List<StoreMst> deleteFranchiseStoreList
+    ) {
+        log.info("PlatformStoreMgtController::franchiseDelete called");
+        platformMainService.deleteFranchiseStoreAllList(deleteFranchiseStoreList);
+
+        return deleteFranchiseStoreList;
     }
 
 
--- src/main/java/org/minideliveryproject/application/platform/service/PlatformMainService.java
+++ src/main/java/org/minideliveryproject/application/platform/service/PlatformMainService.java
@@ -5,14 +5,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.minideliveryproject.application.domain.entity.FranchiseMst;
 import org.minideliveryproject.application.domain.entity.StoreMst;
 import org.minideliveryproject.application.domain.entity.UserMst;
+import org.minideliveryproject.application.domain.entity.embeded.DeleteType;
 import org.minideliveryproject.application.domain.repository.FranchiseMstRepository;
 import org.minideliveryproject.application.domain.repository.StoreMstRepositoryImpl;
+import org.minideliveryproject.application.domain.repository.UserMstRepositoryImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,20 +26,79 @@ import java.util.Optional;
 public class PlatformMainService {
 
     private final StoreMstRepositoryImpl storeMstRepository;
+    private final FranchiseMstRepository franchiseMstRepository;
+    private final UserMstRepositoryImpl userMstRepository;
 
     /**
-     * 프랜차이즈 점포 find All to JSON
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 조회버튼
+     * @param
+     * @return
      */
     public List<StoreMst> selectFranchiseStoreAllList() {
         log.info("PlatformMainService::selectFranchiseAllList called");
-        List<StoreMst> franchiseStoreAllList = null;
+        List<StoreMst> franchiseStoreAllList = storeMstRepository.findAll();
+        List<StoreMst> franchiseSearchList = new ArrayList<>();
 
-        for (StoreMst storeMst : storeMstRepository.findAll()) {
-            System.out.println("storeMst = " + storeMst);
+        for (int i = 0; i < franchiseStoreAllList.size(); i++) {
+            if (franchiseStoreAllList.get(i).getFranchiseMst() != null)
+                franchiseSearchList.add(franchiseStoreAllList.get(i));
         }
 
-        return franchiseStoreAllList;
+        return franchiseSearchList;
     }
 
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 등록버튼
+     */
+    public List<StoreMst> createFranchiseStoreAllList(List<StoreMst> createFranchiseStoreList) {
+        log.info("PlatformMainService::createFranchiseStoreAllList called");
+
+        // STORE_MST, FRANCHISE_MST, USER_MST 저장
+        storeMstRepository.saveAll(createFranchiseStoreList);
+        for (int i = 0; i < createFranchiseStoreList.size(); i++) {
+            franchiseMstRepository.save(createFranchiseStoreList.get(i).getFranchiseMst());
+            userMstRepository.save(createFranchiseStoreList.get(i).getUserMst());
+        }
+
+        return createFranchiseStoreList;
+    }
+
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * 수정버튼
+     * @param updateFranchiseStoreList
+     * @return
+     */
+    public List<StoreMst> updateFranchiseStoreAllList(List<StoreMst> updateFranchiseStoreList) {
+        log.info("PlatformMainService::updateFranchiseStoreAllList called");
+
+        // STORE_MST, FRANCHISE_MST, USER_MST 저장
+        storeMstRepository.saveAll(updateFranchiseStoreList);
+        for (int i = 0; i < updateFranchiseStoreList.size(); i++) {
+            franchiseMstRepository.save(updateFranchiseStoreList.get(i).getFranchiseMst());
+            userMstRepository.save(updateFranchiseStoreList.get(i).getUserMst());
+        }
+
+        return updateFranchiseStoreList;
+    }
+
+    /**
+     * 플랫폼 > 점포관리 > 프랜차이즈 점포
+     * DELETE_TYPE = NO 수정
+     * 삭제버튼
+     */
+    public List<StoreMst> deleteFranchiseStoreAllList(List<StoreMst> deleteFranchiseStoreList) {
+        log.info("PlatformMainService::deleteFranchiseStoreAllList called");
+
+        for (int i = 0; i < deleteFranchiseStoreList.size(); i++) {
+            List<StoreMst> deleteStoreList = deleteFranchiseStoreList;
+            deleteStoreList.get(i).setDeleteType(DeleteType.YES);
+            storeMstRepository.save(deleteStoreList.get(i));
+        }
+
+        return deleteFranchiseStoreList;
+    }
 
 }
