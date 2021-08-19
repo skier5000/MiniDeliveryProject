fcc4fc430d418832640cb6e8ac115a8475ac2f6b
Lee JunBeon <skier5000@naver.com>
2021-08-19 21:52:12 +0900

    QueryDSL 테스트 시작

--- src/test/java/org/minideliveryproject/application/domain/repository/DbQueryDslTest.java
+++ src/test/java/org/minideliveryproject/application/domain/repository/DbQueryDslTest.java
@@ -21,22 +21,22 @@ public class DbQueryDslTest {
     /*
 QueryDSL 테스트
 */
-    @Test
-    public void group() throws Exception {
-        List<Tuple> fetch =
-                queryFactory.select(team.name, member.age.avg())
-                        .from(member)
-                        .join(member.team, team)
-                        .groupBy(team.name)
-                        .fetch();
-
-        Tuple teamA = fetch.get(0);
-        Tuple teamB = fetch.get(1);
-
-        assertThat(teamA.get(team.name)).isEqualTo("TEAM A");
-        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
-
-        assertThat(teamB.get(team.name)).isEqualTo("TEAM B");
-        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
-    }
+//    @Test
+//    public void group() throws Exception {
+//        List<Tuple> fetch =
+//                queryFactory.select(team.name, member.age.avg())
+//                        .from(member)
+//                        .join(member.team, team)
+//                        .groupBy(team.name)
+//                        .fetch();
+//
+//        Tuple teamA = fetch.get(0);
+//        Tuple teamB = fetch.get(1);
+//
+//        assertThat(teamA.get(team.name)).isEqualTo("TEAM A");
+//        assertThat(teamA.get(member.age.avg())).isEqualTo(15);
+//
+//        assertThat(teamB.get(team.name)).isEqualTo("TEAM B");
+//        assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+//    }
 }
