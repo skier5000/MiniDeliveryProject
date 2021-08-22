58a3d444a96bd5b4a01310bdd86b8bf3f56c3a25
Lee JunBeon <skier5000@naver.com>
2021-08-22 14:15:25 +0900

    native Query 메소드 추가

--- src/main/java/org/minideliveryproject/application/domain/repository/OrderMstRepository.java
+++ src/main/java/org/minideliveryproject/application/domain/repository/OrderMstRepository.java
@@ -19,27 +19,67 @@ public interface OrderMstRepository extends JpaRepository<OrderMst, Long> {
 
     public Optional<OrderMst> findByPayment(PaymentType paymentType);
 
+    @Query(nativeQuery = true, value = "" +
+            "SELECT sm.STORE_TYPE AS STORE_TYPE" +
+            "     , om.STORE_MST_SEQ AS STORE_MST_SEQ" +
+            "     , sm.STORE_NAME AS STORE_NAME" +
+            "     , sm.STORE_TEL AS STORE_TEL" +
+            "     , sm.CONT_RENEW_DATE AS CONT_DATE" +
+            "     , COUNT(om.STORE_MST_SEQ) AS ALL_ORDER" +
+            "     , COUNT(CASE WHEN sm.DELETE_TYPE= 'NO' THEN 1 END) AS ALL_ORDER_DELETE_NO" +
+            "     , COUNT(CASE WHEN sm.DELETE_TYPE= 'YES' THEN 1 END) AS ALL_ORDER_DELETE_YES" +
+            "  FROM ORDER_MST om" +
+            "    INNER JOIN ORDER_DETAIL od" +
+            "        ON om.ORDER_MST_SEQ = od.ORDER_MST_SEQ" +
+            "    INNER JOIN STORE_MST sm" +
+            "        ON om.STORE_MST_SEQ = sm.STORE_MST_SEQ" +
+            " WHERE 1=1" +
+            "   AND TO_CHAR(om.ORDER_DATE, 'YYYYMMDD') BETWEEN ?1 AND ?2" +
+            "   AND sm.STORE_NAME LIKE %?3%"+
+            " GROUP BY om.STORE_MST_SEQ"
+    )
+    public List<OrderMstDto> findByStartEndStoreNm(String startContract, String endContract, String storeNm);
+
+    @Query(nativeQuery = true, value = "" +
+            "SELECT sm.STORE_TYPE AS STORE_TYPE" +
+            "     , om.STORE_MST_SEQ AS STORE_MST_SEQ" +
+            "     , sm.STORE_NAME AS STORE_NAME" +
+            "     , sm.STORE_TEL AS STORE_TEL" +
+            "     , sm.CONT_RENEW_DATE AS CONT_DATE" +
+            "     , COUNT(om.STORE_MST_SEQ) AS ALL_ORDER" +
+            "     , COUNT(CASE WHEN sm.DELETE_TYPE= 'NO' THEN 1 END) AS ALL_ORDER_DELETE_NO" +
+            "     , COUNT(CASE WHEN sm.DELETE_TYPE= 'YES' THEN 1 END) AS ALL_ORDER_DELETE_YES" +
+            "  FROM ORDER_MST om" +
+            "    INNER JOIN ORDER_DETAIL od" +
+            "        ON om.ORDER_MST_SEQ = od.ORDER_MST_SEQ" +
+            "    INNER JOIN STORE_MST sm" +
+            "        ON om.STORE_MST_SEQ = sm.STORE_MST_SEQ" +
+            " WHERE 1=1" +
+            "   AND sm.STORE_MST_SEQ = ?1" +
+            " GROUP BY om.STORE_MST_SEQ"
+    )
+    public List<OrderMstDto> findByStoreCode(Long storeCode);
 
 
-    @Query(value = "SELECT om.ORDER_MST_SEQ" +
-            "            , om.STORE_MST_SEQ" +
-            "            , sm.STORE_TYPE" +
-            "            , sm.STORE_NAME" +
-            "            , sm.STORE_TEL" +
-            "            , sm.CONT_DATE" +
-            "         FROM ORDER_MST om" +
-            "           INNER JOIN ORDER_DETAIL od" +
-            "               ON om.ORDER_MST_SEQ = od.ORDER_MST_SEQ" +
-            "           INNER JOIN STORE_MST sm" +
-            "               ON om.STORE_MST_SEQ = sm.STORE_MST_SEQ" +
-            "        WHERE 1=1"
-            , nativeQuery = true)
-    public <S extends OrderMst> Iterable<S> testFindByOrderMstSearchDto();
-
-    @Query(nativeQuery = true, value =
-            "SELECT ORDER_MST_SEQ" +
-            "     , COUNT(*) AS CNT" +
-            "  FROM ORDER_MST" +
-            " GROUP BY ORDER_MST_SEQ")
-    public <S extends OrderMst> Iterable<S> testFindByOrderMstSearchDto2();
+//    @Query(value = "SELECT om.ORDER_MST_SEQ" +
+//            "            , om.STORE_MST_SEQ" +
+//            "            , sm.STORE_TYPE" +
+//            "            , sm.STORE_NAME" +
+//            "            , sm.STORE_TEL" +
+//            "            , sm.CONT_DATE" +
+//            "         FROM ORDER_MST om" +
+//            "           INNER JOIN ORDER_DETAIL od" +
+//            "               ON om.ORDER_MST_SEQ = od.ORDER_MST_SEQ" +
+//            "           INNER JOIN STORE_MST sm" +
+//            "               ON om.STORE_MST_SEQ = sm.STORE_MST_SEQ" +
+//            "        WHERE 1=1"
+//            , nativeQuery = true)
+//    public <S extends OrderMst> Iterable<S> testFindByOrderMstSearchDto();
+//
+//    @Query(nativeQuery = true, value =
+//            "SELECT ORDER_MST_SEQ" +
+//            "     , COUNT(*) AS CNT" +
+//            "  FROM ORDER_MST" +
+//            " GROUP BY ORDER_MST_SEQ")
+//    public <S extends OrderMst> Iterable<S> testFindByOrderMstSearchDto2();
 }
