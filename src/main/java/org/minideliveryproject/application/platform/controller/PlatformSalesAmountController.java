20f9bea0e41469e9e641bf80040ae93c4c3d6575
Lee JunBeon <skier5000@naver.com>
2021-08-24 15:54:18 +0900

    SalesAmount Controller/Service 추가

--- src/main/java/org/minideliveryproject/application/platform/controller/PlatformSalesAmountController.java
+++ src/main/java/org/minideliveryproject/application/platform/controller/PlatformSalesAmountController.java
@@ -2,10 +2,15 @@ package org.minideliveryproject.application.platform.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.minideliveryproject.application.dto.OrderMstDto;
 import org.minideliveryproject.application.platform.service.PlatformSalesAmountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -17,10 +22,23 @@ public class PlatformSalesAmountController {
 
     /**
      * 플랫폼 > 점포관리 > 매출관리
+     *
      * @return
      */
     @GetMapping(value = "/")
-    public String indexStoreMgtOrder() {
+    public String indexStoreMgtSalesAmount() {
         return "platform/storeMgt/salesAmount";
+    }
+
+
+    @ResponseBody
+    @GetMapping("/search")
+    public void orderMstSearch(
+            @RequestParam(value = "startContract", required = false) String startContract,
+            @RequestParam(value = "endContract", required = false) String endContract,
+            @RequestParam(value = "storeCode", required = false) Long storeCode,
+            @RequestParam(value = "storeNm", required = false) String storeNm
+    ) {
+        log.info("PlatformOrderController::orderMstSearch called");
     }
 }
