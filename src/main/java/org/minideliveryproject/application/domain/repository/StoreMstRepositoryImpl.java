83fe3f2f2ff19c53ee4c118ed461c2d93a32a4ff
Lee JunBeon <skier5000@naver.com>
2021-09-04 15:15:07 +0900

    Native Query 추가

--- src/main/java/org/minideliveryproject/application/domain/repository/StoreMstRepositoryImpl.java
+++ src/main/java/org/minideliveryproject/application/domain/repository/StoreMstRepositoryImpl.java
@@ -1,6 +1,7 @@
 package org.minideliveryproject.application.domain.repository;
 
 import org.minideliveryproject.application.domain.entity.StoreMst;
+import org.minideliveryproject.application.platform.dto.StoreContractMstDto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +17,18 @@ public interface StoreMstRepositoryImpl extends JpaRepository<StoreMst, Long> {
 
     @Query("SELECT sm FROM StoreMst sm WHERE sm.storeName LIKE %?1%")
     public List<StoreMst> findByStoreNameLike(String storeName);
+
+    @Query(nativeQuery = true, value = "" +
+            "SELECT A.STORE_MST_SEQ     AS storeMstSeq" +
+            "     , A.STORE_TYPE        AS storeType" +
+            "     , A.STORE_NAME        AS storeName" +
+            "     , A.CITY              AS city" +
+            "     , A.CONT_DATE         AS contDate" +
+            "     , A.CONT_EXP_DATE     AS contExpDate" +
+            "     , A.CONT_RENEW_DATE   AS contRenewDate" +
+            "     , A.STORE_TEL         AS storeTel" +
+            "  FROM STORE_MST A" +
+            " WHERE 1=1" +
+            "   AND TO_CHAR(CONT_DATE, 'YYYYMMDD') BETWEEN :startDate AND :endDate")
+    public List<StoreContractMstDto> findByStoreMstContractDate(@Param(value = "startDate") String startDate, @Param(value = "endDate") String endDate);
 }
