e4b16f70da22e6590bf145ea8540969a8b466b7d
Lee JunBeon <skier5000@naver.com>
2021-09-05 13:59:59 +0900

    PlatformStoreContractService 추가

--- src/main/java/org/minideliveryproject/application/platform/service/PlatformStoreContractService.java
+++ src/main/java/org/minideliveryproject/application/platform/service/PlatformStoreContractService.java
@@ -14,6 +14,7 @@ import java.math.BigInteger;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -39,10 +40,70 @@ public class PlatformStoreContractService {
         List<StoreContractMstDto> allStoreContractMstList = storeMstRepository.findByStoreMstContractDate(startDate, endDate);
 
         if (storeCode != null) {
-            List<StoreContractMstDto> storeContractMstDtoStream = allStoreContractMstList.stream().filter(storeContractMstDto -> storeContractMstDto.getStoreMstSeq().equals(BigInteger.valueOf(storeCode))).collect(Collectors.toList());
+            List<StoreContractMstDto> storeContractMstDtoStream =
+                    allStoreContractMstList
+                            .stream()
+                            .filter(storeContractMstDto -> storeContractMstDto.getStoreMstSeq().equals(BigInteger.valueOf(storeCode)))
+                            .collect(Collectors.toList());
+
             storeContractMstList.addAll(storeContractMstDtoStream);
         } else {
-            if (storeType != null && storeNm == null && city == null) {
+            if (storeType != null) {
+                List<StoreContractMstDto> storeContractMstDtoStream = allStoreContractMstList
+                        .stream()
+                        .filter(storeContractMstDto -> storeContractMstDto.getStoreType().equals(storeType))
+                        .collect(Collectors.toList());
+                if (storeNm == null && city != null) {
+                    storeContractMstDtoStream = storeContractMstDtoStream
+                            .stream()
+                            .filter(storeContractMstDto -> storeContractMstDto.getCity().equals(city))
+                            .collect(Collectors.toList())
+                    ;
+                } else if (storeNm != null) {
+                    storeContractMstDtoStream = storeContractMstDtoStream
+                            .stream()
+                            .filter(storeContractMstDto -> storeContractMstDto.getStoreName().contains(storeNm))
+                            .collect(Collectors.toList());
+                    if (city != null) {
+                        storeContractMstDtoStream = storeContractMstDtoStream
+                                .stream()
+                                .filter(storeContractMstDto -> storeContractMstDto.getCity().equals(city))
+                                .collect(Collectors.toList());
+                    }
+                }
+
+                storeContractMstList.addAll(storeContractMstDtoStream);
+
+            } else if (storeType == null) {
+                if (storeNm != null) {
+                    List<StoreContractMstDto> storeContractMstDtoStream =
+                            allStoreContractMstList
+                                    .stream()
+                                    .filter(storeContractMstDto -> storeContractMstDto.getStoreMstSeq().equals(storeNm))
+                                    .collect(Collectors.toList());
+
+                    if (city == null) {
+                        storeContractMstList.addAll(storeContractMstDtoStream);
+                    } else if (city != null) {
+                        storeContractMstDtoStream = storeContractMstDtoStream
+                                .stream()
+                                .filter(storeContractMstDto -> storeContractMstDto.getCity().equals(city))
+                                .collect(Collectors.toList());
+                        storeContractMstList.addAll(storeContractMstDtoStream);
+                    }
+                }
+                else if (storeNm == null) {
+                    List<StoreContractMstDto> storeContractMstDtoStream = allStoreContractMstList;
+                    if (city == null) {
+                        storeContractMstList.addAll(storeContractMstDtoStream);
+                    } else if (city != null) {
+                        storeContractMstDtoStream = storeContractMstDtoStream
+                                .stream()
+                                .filter(storeContractMstDto -> storeContractMstDto.getCity().equals(city))
+                                .collect(Collectors.toList());
+                        storeContractMstList.addAll(storeContractMstDtoStream);
+                    }
+                }
 
             }
         }
